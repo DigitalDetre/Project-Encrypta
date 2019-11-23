edb70120ff6db04b6fefdd504d3f5058988c9101
Jake <jrbelga@gmail.com>
2019-11-23 01:21:41 -0500

    made minor aesthetic changes

--- app/src/main/java/com/example/encryptaapplication/Activities/LoginActivity.java
+++ app/src/main/java/com/example/encryptaapplication/Activities/LoginActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -38,7 +39,6 @@ public class LoginActivity extends AppCompatActivity {
 
         String entered_name = username.getText().toString();
         String entered_password = password.getText().toString();
-
 
         // cancel takes user back to main page
         cancel_btn.setOnClickListener(new View.OnClickListener() {
