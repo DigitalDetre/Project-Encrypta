6120068ae8a86a0f8a31148ea2d211ec73b22174
joshu <joshuafrank@knights.ucf.edu>
2019-11-25 01:44:56 -0500

    initial Setup for user settings

---
+++ app/src/main/java/com/example/encryptaapplication/Activities/UserSettingsActivity.java
@@ -0,0 +1,16 @@
+package com.example.encryptaapplication.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.encryptaapplication.R;
+
+public class UserSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_settings);
+    }
+}
