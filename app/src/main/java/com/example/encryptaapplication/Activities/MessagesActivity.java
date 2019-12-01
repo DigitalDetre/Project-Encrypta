cf337e49e43af8cd22634123d9b3c59f39559e8f
Jamil Gonzalez <jamil.gonzalez@icloud.com>
2019-12-01 12:14:39 -0500

    added drop down in deletion settings

--- app/src/main/java/com/example/encryptaapplication/Activities/MessagesActivity.java
+++ app/src/main/java/com/example/encryptaapplication/Activities/MessagesActivity.java
@@ -1,12 +1,15 @@
 package com.example.encryptaapplication.Activities;
 
 
+import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.encryptaapplication.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class MessagesActivity extends AppCompatActivity{
@@ -14,6 +17,8 @@ public class MessagesActivity extends AppCompatActivity{
     private TextView window_text, policy_text;
     private Spinner spinner;
     private Button change_btn;
+
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +29,16 @@ public class MessagesActivity extends AppCompatActivity{
         policy_text = findViewById(R.id.policy_text);
         spinner = findViewById(R.id.spinner);
         change_btn = findViewById(R.id.change_btn);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("Messages");
+
+        change_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final int time;
+
+            }
+        });
     }
+
 }
