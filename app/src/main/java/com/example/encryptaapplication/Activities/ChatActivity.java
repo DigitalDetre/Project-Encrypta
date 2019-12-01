312a67d231c471287bbd7b2c295a690e25773b3d
Jamil Gonzalez <jamil.gonzalez@icloud.com>
2019-11-30 23:42:08 -0500

    changes to chat activity (contains null pointer bug in chatActivity

--- app/src/main/java/com/example/encryptaapplication/Activities/ChatActivity.java
+++ app/src/main/java/com/example/encryptaapplication/Activities/ChatActivity.java
@@ -1,32 +1,78 @@
 package com.example.encryptaapplication.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.encryptaapplication.Entities.User;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.encryptaapplication.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ChatActivity extends AppCompatActivity {
+
+    TextView name;
+    CircleImageView profile_pic;
+    DatabaseReference reference;
+    FirebaseUser user;
+    Intent intent;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        setContentView(R.layout.activity_chat);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                finish();
+            }
+        });
+
+        profile_pic = findViewById(R.id.profile_pic);
+        name = findViewById(R.id.username);
+
+        intent = getIntent();
+
+        if (intent != null) {
+            final String userid = intent.getStringExtra("userid");
+
+            user = FirebaseAuth.getInstance().getCurrentUser();
+            reference = FirebaseDatabase.getInstance().getReference("Users").child(userid);
+        }
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                name.setText(user.getName());
+                profile_pic.setImageResource(R.mipmap.default_icon);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
     }
